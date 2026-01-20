b07b47995d9adad098e52686c528db149fd91305
Tom Steigerwald <steigerwald@gmail.com>
2026-01-20 05:46:08 +0000

    fix: disable ANSI colors when not running in a TTY

--- lib/core.sh
+++ lib/core.sh
@@ -19,16 +19,26 @@ MYSQL_CONTAINER="${DBX_MYSQL_CONTAINER:-mysql-dbx}"
 KEYCHAIN_SERVICE="dbx"
 
 # ============================================================================
-# Colors
+# Colors (disabled if not a TTY)
 # ============================================================================
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-CYAN='\033[0;36m'
-NC='\033[0m' # No Color
-BOLD='\033[1m'
+if [[ -t 1 ]]; then
+  RED='\033[0;31m'
+  GREEN='\033[0;32m'
+  YELLOW='\033[0;33m'
+  BLUE='\033[0;34m'
+  CYAN='\033[0;36m'
+  NC='\033[0m'
+  BOLD='\033[1m'
+else
+  RED=''
+  GREEN=''
+  YELLOW=''
+  BLUE=''
+  CYAN=''
+  NC=''
+  BOLD=''
+fi
 
 # ============================================================================
 # Logging Functions
